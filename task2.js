873d336ccf441963748eea039e53c2803530f19c
Dmitry <kererelore14@gmail.com>
2026-06-28 10:47:17 +0300

    add calc function, explore that

--- task2.js
+++ task2.js
@@ -3,3 +3,24 @@ console.log(x)
 
 let y = 'hello'
 console.log(y)
+
+
+// you mast write a comment about this function. What she do?
+// and also use this function below please)
+function calc(x, y, operator) {
+    switch (operator) {
+        case '+':
+            return x + y;
+        case '-':
+            return x - y;
+        case '/':
+            return x / y;
+        case '*':
+            return x * y;
+    }
+}
+
+
+
+// let res = calc(...)
+// console.log(...)
